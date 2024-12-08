16e9ac097fd4b56cb232c6aa36758e3873d1b0d6
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-12-08 12:03:35 +0700

    update: nextjs 15 app router 🌼

---
+++ src/app/template.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>
+}
---
+++ src/components/shared/Loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className='fixed inset-0 flex items-center justify-center'>Loading...</div>
+}
